dcf3e7ee1a9ce31a0801cdce67260cb663c5fba6
rafaelfachinelli <rafael.l.a.fachinelli@gmail.com>
2025-05-03 16:51:52 -0300

    feat: add project pages for Flex Sewing Machine, Leroy Merlin, and Markit3D with internationalization support

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import Link from 'next/link';
 
-import { cn } from '@/lib/utils';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -112,13 +111,19 @@ export function Navbar() {
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {projects.map((project) => (
-                <ListItem
-                  key={project.title}
-                  title={project.title}
-                  href={`/${lang}/${project.href}`}
-                >
-                  {project.description}
-                </ListItem>
+                <NavigationMenuLink asChild key={project.title}>
+                  <Link
+                    href={`/${lang}/${project.href}`}
+                    className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    <div className="text-sm font-medium leading-none">
+                      {project.title}
+                    </div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                      {project.description}
+                    </p>
+                  </Link>
+                </NavigationMenuLink>
               ))}
             </ul>
           </NavigationMenuContent>
@@ -137,29 +142,3 @@ export function Navbar() {
     </NavigationMenu>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, href, ...props }, ref) => {
-  return (
-    <li>
-      <Link
-        ref={ref}
-        className={cn(
-          'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-          className
-        )}
-        href={href ?? '/'}
-        {...props}
-      >
-        <div className="text-sm font-medium leading-none">{title}</div>
-        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-          {children}
-        </p>
-      </Link>
-    </li>
-  );
-});
-
-ListItem.displayName = 'ListItem';
